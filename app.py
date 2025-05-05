1554ac486629dbbd856aa62e9f45aa0f0b912e25
-Juan-525 <parganjuanfelipe83@gmail.com>
2025-05-05 23:33:35 +0800

    Fix deployment structure

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def home():
     return "John Philip Pargan BSIT - 3 B"
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))
-   app.run (host='0.0.0.0', port=port)
+   app.run(host="0.0.0.0",
+           port= int(os.environ.get("PORT", 8000)))
