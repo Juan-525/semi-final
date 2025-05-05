857c2947eb49df68cd442f793a35d9b828d1ed8f
-Juan-525 <parganjuanfelipe83@gmail.com>
2025-05-05 23:04:26 +0800

    third commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
+
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/') 
 def home():
     return "John Philip Pargan BSIT - 3 B"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+   port = int(os.environ.get("PORT", 5000))
+   app.run (host='0.0.0.0', port=port)
