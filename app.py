a21add9f21e9d513d0792a6c12e545200bafa66c
-Juan-525 <parganjuanfelipe83@gmail.com>
2025-05-05 21:59:11 +0800

    Fix app.py host and port for Railway

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "John Philip Pargan BSIT - 3 B"
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
